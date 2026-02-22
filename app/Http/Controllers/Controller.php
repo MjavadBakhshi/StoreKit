a4bd7360250efc9e1f4857718ec9319a44f94710
mjavad <mjavad.bakhshi@gmail.com>
2026-02-22 22:18:07 +0330

    Add shared response and controller logic in default laravel Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Domain\Shared\Exceptions\ActionException;
+
 abstract class Controller
 {
-    //
+
+    function isActionExeption(mixed &$result) :bool
+    {
+        return $result instanceof $result;
+    }
+
+
+    function successResponse(array $data = [], string $message = "", int $status = 200)
+    {
+        return response()->json([
+                'ok' => true,
+                'data' => $data,
+                'message' => $message
+            ], $status);
+    }
+
+    function failedResponse(array $data = [], string $message = "", int $status = 401)
+    {
+        return response()->json([
+                'error' => true,
+                'data' => $data,
+                'message' => $message
+            ], $status);
+    }
+
+
+
 }
