555076d1173af317f2fcede832ab90369bbbd514
mjavad <mjavad.bakhshi@gmail.com>
2026-03-02 02:01:04 +0330

    Add TDD feature tests for shop products listing

---
+++ tests/Feature/Catalog/Shop/ShopProductListTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Catalog\Shop;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ShopProductListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    public function customer_can_browse_store_products(): void
+    {
+
+    }
+
+    #[Test]
+    public function customer_broswes_correct_store_products() :void
+    {
+
+    }
+
+    #[Test]
+    public function customer_can_browse_active_products() :void
+    {
+
+    }
+
+    #[Test]
+    public function customer_can_visit_store_product() :void
+    {
+        
+    }
+
+}
