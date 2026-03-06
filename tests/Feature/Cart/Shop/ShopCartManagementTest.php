57ded0186013e73ca3d20225c914df692665e2d2
mjavad <mjavad.bakhshi@gmail.com>
2026-03-06 19:54:19 +0330

    Add TDD feature tests for creting and updating a cart

---
+++ tests/Feature/Cart/Shop/ShopCartManagementTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Cart\Shop;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class ShopCartManagementTest extends TestCase
+{
+    #[Test]
+    function customer_can_add_new_active_item_to_cart_successfully()
+    {}
+
+    #[Test]
+    function customer_can_increase_item_quantity()
+    {}
+
+    #[Test]
+    function customer_can_decrease_item_quantity()
+    {}
+
+    #[Test]
+    function cart_is_removed_when_there_is_no_items()
+    {}
+    
+}
