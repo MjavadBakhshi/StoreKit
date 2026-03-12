abf49e8fecc8a3fa8ef7b055a380478aa90e7a37
mjavad <mjavad.bakhshi@gmail.com>
2026-03-12 14:05:30 +0330

    Add TDD feature tests for upserting product variants

---
+++ tests/Feature/Catalog/UpsertProductVariantTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature\Catalog;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+class UpsertProductVariantTest extends TestCase
+{
+    use RefreshDatabase;
+
+    #[Test]
+    function insert_new_variant_successfully()
+    {
+
+    }
+
+    #[Test]
+    function update_new_variant_successfully()
+    {
+
+    }
+
+    #[Test]
+    function delete_variants_successfully()
+    {
+
+    }
+
+    #[Test]
+    function store_owner_cannot_access_other_store_products()
+    {
+        
+    }
+}
